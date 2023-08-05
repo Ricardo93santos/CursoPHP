930a8b784d3ebf31afefb606d0b9de82e4dabb6b
Ricardo Santos <ricardo93ssantos@gmail.com>
2023-08-05 06:10:34 -0300

    Alteração na extrutura do index.php, para organizar melhor o código e adicionar alguns comentários relevantes.

--- Desafios/des03/des3.3/php/index.php
+++ Desafios/des03/des3.3/php/index.php
@@ -11,36 +11,40 @@
     <h1>Conversor de Moedas V1.0</h1>
   </header>
   <main>
-    <?php 
-      $valor = (float)$_POST["valor"] ?? 0;
+    <?php
 
-    
+      //Valor do dolar, cotação manual via Google
+      $valor_dolar = 5.22;
+
+      //Valor do real, inserido pelo usuário via POST
+      $valor_real = (float)$_POST["valor"] ?? 0;
+    /*
       //Conversão usando a função number_format para fazer formatação monetária.
 
-      function converterMoeda($valor){
-        $valorDolar = 5.22;
-        $cambio = $valor/$valorDolar;
+      function converterMoeda($valor_real, $valor_dolar){
+        $cambio = $valor_real/$valor_dolar;
         return number_format($cambio, 2, ",", ".");
       }
-      $cambio = converterMoeda($valor);
-      $valor_real = number_format($valor, 2, ",", ".");
+      $cambio = converterMoeda($valor_real, $valor_dolar);
+      
+      $real_format = number_format($valor_real, 2, ",", ".");
 
-      echo "<p> Seus R$$valor_real equivalem a US$$cambio</p>";
-
-     /* 
+      echo "<p> Seus R$$real_format equivalem a US$$cambio</p>";
+    */
+     
       //Conversão usando a biblioteca intil, para formatação monetária
+      function converterMoeda($valor_real, $valor_dolar){
+
+        $cambio = $valor_real/$valor_dolar;
+        
+        return $cambio;
+      }
+      $cambio = converterMoeda($valor_real, $valor_dolar);
 
       $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY); //Para alterar o padrão basta trocar a string do idioma do país, "us", "pt_PT", "ru_RU
 
-      function converterMoeda($valor){
-        $valorDolar = 5.22;
-        $cambio = $valor/$valorDolar;
-        return $cambio;
-      }
-      $cambio = converterMoeda($valor);
-
       echo "<p> Seus" .  numfmt_format_currency($padrao, $valor, "BRL") ." equivalem a " . numfmt_format_currency($padrao, $cambio, "USD") ."</p>";
-      */
+  
     ?>
 
     <p><strong>*Cotação fixa de R$5,22</strong></p>
