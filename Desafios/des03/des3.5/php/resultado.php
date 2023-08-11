86155152e158ad1fbe027d9d638a378af758afd5
Ricardo Santos <ricardo93ssantos@gmail.com>
2023-08-11 12:31:12 -0300

    Criação do desafio 3.5

---
+++ Desafios/des03/des3.5/php/resultado.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Analisador de Números Real - Resultado</title>
+  <script src="../js/script.js"></script>
+</head>
+<body>
+
+  <input type="button" id="btnVoltar" value="Voltar">
+</body>
+</html>
