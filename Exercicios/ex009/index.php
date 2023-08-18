e74dabd352704cba59a7d85e456cf6bc0f2d0b1b
Ricardo Santos <ricardo93ssantos@gmail.com>
2023-08-18 13:54:11 -0300

    Criação do exercício 09, sobre Superglobais no PHP.

---
+++ Exercicios/ex009/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Superglobais no PHP</title>
+</head>
+<body>
+  <h1>Superglobais PHP</h1>
+  <p>Superglobais no PHP são arrays especiais que armazenam informações globais do ambiente do servidor e da solicitação HTTP, exitem 8 superglobais no PHP. Elas são automaticamente acessíveis de qualquer parte do script PHP, sem a necessidade de declaração adicional.</p>
+  <p>As superglobais são:</p>
+  <ol>
+    <li><strong>$_GET</strong>: Usada para coletar enviados para o script via paraametros de URL, é frequentimente usada para acessar dados de consulta em URLs. Ex: <strong>$_GET['nome']</strong></li>
+    <li><strong>$_POST</strong>: Usada para coletar dados enviados por um formulário HTML, semelhante ao metodo GET, esses dados são invisíveis na URL, o que é útil para informações sensíveis. Ex: <strong>$_POST['senha']</strong></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ol>
+</body>
+</html>
