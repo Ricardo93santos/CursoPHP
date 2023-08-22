ffc8fe2c208b0cd70328b1e6de3db4a992336c56
Ricardo Santos <ricardo93ssantos@gmail.com>
2023-08-22 12:28:50 -0300

    Adição do estilo css nos desafios.

--- Desafios/des04/des4.5/php/resultado.php
+++ Desafios/des04/des4.5/php/resultado.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Analisador de Números Real - Resultado</title>
+  <link rel="stylesheet" href="../css/style.css">
   <script src="../js/script.js"></script>
 </head>
 <body>
