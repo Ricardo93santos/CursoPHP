7a65f908d6e925dce200be025e02c9785e1462d5
Ricardo Santos <ricardo93ssantos@gmail.com>
2023-08-23 13:06:50 -0300

    Inicio do desafio 006, falta formatar os valores na moeda real

---
+++ Desafios/des06/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Cálculo Salarial</title>
+</head>
+<body>
+  <?php 
+    $salarioMinimo = 1380.00
+  ?>
+  <main>
+    <h1>Informe seu salário</h1>
+    <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+      <label for="salario">Salário (R$)</label>
+      <input type="number" name="salario" min="0" id="salario" step="0.01" value = "0">
+      <p>Considerando o salário minimo de <strong>R$<?=number_format($salarioMinimo, 2, ",", ".")?></strong></p>
+      <input type="submit" value="Calcular">
+    </form>
+  </main>
+  <section>
+      <h2>Resultado Final</h2>
+      <?php
+        $salario = $_POST["salario"];
+        $quantidadeDeSalariosMinimo = intdiv($salario, $salarioMinimo);
+
+        $fracaoDoSalario = $salario % $salarioMinimo;
+
+        echo "O seu salário é de $salario e a quantidade de salários minimo que voce recebe é de $quantidadeDeSalariosMinimo + $fracaoDoSalario";
+      ?>
+    </section>
+</body>
+</html>
