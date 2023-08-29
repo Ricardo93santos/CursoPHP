7956f37d11a27384660c55e6c29d12f28333e859
Ricardo Santos <ricardo93ssantos@gmail.com>
2023-08-29 13:40:38 -0300

    Criação da aula 005, onde foi tratado os operadores de comparação e operadores lógicos

---
+++ Aulas/aula005/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Operadores de Comparação e Operadores Lógicos no PHP</title>
+</head>
+<body>
+  <p>No PHP, os operadores de comparação e os operadores lógicos são distintos, embora ambos sejam frequentemente utilizados em conjunto para criar expressões condicionais e realizar testes lógicos.</p>
+  <h2>Operadores de Comparação</h2>
+  <p>Os operadores de comparação são usados para comparar valores e determinar a relação entre eles. Eles retornam um valor booleano (verdadeiro ou falso) com base na comparação realizada. Alguns exemplos de operadores de comparação em PHP são:</p>
+  <ul>
+    <li>== (igual a)</li>
+    <li>!= ou <> (diferente de)</li>
+    <li>< (menor que)</li>
+    <li>> (maior que)</li>
+    <li><= (menor ou igual a)</li>
+    <li>>= (maior ou igual a)</li>
+    <li>=== (igual a em valor e tipo)</li>
+    <li>!== (diferente em valor ou tipo)</li>
+  </ul>
+  <p>Esses operadores são usados para comparar variáveis, valores numéricos, strings, entre outros tipos de dados.</p>
+  <h2>Operadores Lógicos</h2>
+  <p>Os operadores lógicos são utilizados para combinar expressões condicionais e testar múltiplas condições ao mesmo tempo. Eles também retornam um valor booleano. Os principais operadores lógicos em PHP são:</p>
+  <ul>
+    <li>&& ou and (E lógico)</li>
+    <li>|| ou or (OU lógico)</li>
+    <li>! ou not (NÃO lógico)</li>
+    <li>xor (OU exclusivo lógico)</li>
+  </ul>
+  <p>Os operadores lógicos são usados para criar expressões condicionais mais complexas, combinando várias comparações para tomar decisões baseadas em múltiplas condições.</p>
+</body>
+</html>
